292188b2b8999610186163332f9b2b447db56fbc
Levi Natanael da Silva <levi.natanael@gmail.com>
2025-08-01 08:22:48 -0300

    Criando repository

---
+++ TaskManager.Core/Interfaces/ITaskRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using TaskManager.Core.Entities;
+
+namespace TaskManager.Core.Interfaces
+{
+    public interface ITaskRepository
+    {
+        IEnumerable<TaskItem> GetAll();
+        TaskItem GetById(Guid id);
+        void Add(TaskItem task);
+        void Update(TaskItem task);
+        void Delete(Guid id);
+    }
+}
---
+++ TaskManager.Infrastructure/Repositories/JsonTaskRepository.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using TaskManager.Core.Entities;
+using TaskManager.Core.Interfaces;
+
+namespace TaskManager.Infrastructure.Repositories
+{
+    public class JsonTaskRepository : ITaskRepository
+    {
+        private readonly string _filePath;
+        private List<TaskItem> _tasks;
+
+        public JsonTaskRepository(string filePath)
+        {
+            _filePath = filePath;
+            _tasks = LoadTasks();
+        }
+
+        private List<TaskItem> LoadTasks()
+        {
+            if (!File.Exists(_filePath))
+                return new List<TaskItem>();
+
+            var json = File.ReadAllText(_filePath);
+            return JsonConvert.DeserializeObject<List<TaskItem>>(json) ?? new List<TaskItem>();
+        }
+
+        private void SaveTasks()
+        {
+            var json = JsonConvert.SerializeObject(_tasks, Formatting.Indented);
+            File.WriteAllText(_filePath, json);
+        }
+
+        public IEnumerable<TaskItem> GetAll() => _tasks;
+
+        public TaskItem GetById(Guid id) => _tasks.FirstOrDefault(t => t.Id == id);
+
+        public void Add(TaskItem task)
+        {
+            _tasks.Add(task);
+            SaveTasks();
+        }
+
+        public void Update(TaskItem task)
+        {
+            var index = _tasks.FindIndex(t => t.Id == task.Id);
+            if (index >= 0)
+            {
+                _tasks[index] = task;
+                SaveTasks();
+            }
+        }
+
+        public void Delete(Guid id)
+        {
+            var task = GetById(id);
+            if (task != null)
+            {
+                _tasks.Remove(task);
+                SaveTasks();
+            }
+        }
+    }
+}
